d27f44a9138c390ec251c27fdde40e4f6c1ae186
James Ferneyhough <gossferneyhough@gmail.com>
2026-06-07 14:25:08 -0400

    Tui Prototype works!!!! more testing/iteration to be done.

---
+++ acp/__init__.py
@@ -0,0 +1,5 @@
+"""ACP (Agent Communication Protocol) bridge for Agent Home.
+
+This module provides a subprocess bridge that speaks ACP (stdio JSON-RPC)
+to clients like Toad, translating to Agent Home's HTTP/SSE API.
+"""
