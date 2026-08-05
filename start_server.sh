5702461facc220abc6b851dec7fbae820d7b1061
James F <gossferneyhough@gmail.com>
2026-08-05 13:11:28 -0400

    add clarifying comment to start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# For starting up server outside a docker container for quick tests. Not intended for proper deployment
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
